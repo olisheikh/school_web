3b25c08bdbd10c73bba873584a50c3a19007c4cb
Torikul Islam Oli <torikulislamoli@MAC-9064C0.local>
2026-01-01 11:18:05 +0100

    chore: add .gitignore file to exclude system files.
    Prevents OS and environment files from being tracked by the git.

--- school_web/urls.py
+++ school_web/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title =
+    )
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('school/', include('student.urls'))
